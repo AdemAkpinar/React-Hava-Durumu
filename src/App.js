7c16d92a09a73ad30871dd138e4b499d36b64915
AdemAkpinar <ademakpinar.bt@gmail.com>
2022-11-17 19:55:33 +0300

    Yenileme
    Yenileme

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       <div className="mb-3 pt-20 ">
         <input
           onChange={(e) => setSearch(e.target.value)}
-          type="text" placeholder="Placeholder" className="px-20 py-1 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring" />
+          type="text" placeholder="Placeholder" className="px-10 py-8 placeholder-slate-300 text-slate-600 relative rounded text-sm text-center border-4 shadow outline-none focus:outline-none focus:ring" />
         <City city={city} />
       </div>
     </div>
--- src/City.jsx
+++ src/City.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
-const City = ({city}) => {
+var tarih = new Date();
+var yil = tarih.getFullYear();
+var ay = tarih.getMonth();
+var gun = tarih.getDay();
+var saat = tarih.getHours();
+var dakika = tarih.getMinutes();
+const City = ({ city }) => {
   return (
     <div>
-      <h1>ŞEHİR: {city.name}</h1>
-      <h1>SICAKLIK: {city.main.temp}</h1>
+      <h1>{gun + "/" + ay + "/" + yil + " " + saat + ":" + dakika}</h1>
+      <h1>ŞEHİR: {city?.name}</h1>
+      <h1>SICAKLIK: {city?.main.temp}</h1>
     </div>
   )
 }
